45713a3eb9db064d3b4359368383e07b08e2d735
DAGwood1018 <43223204+DAGwood1018@users.noreply.github.com>
2024-01-15 15:01:10 -0500

    Removed unneeded DFT class

--- cpp_off_axis_holo_dev/src/bind.cpp
+++ cpp_off_axis_holo_dev/src/bind.cpp
@@ -1,15 +1,7 @@
-#include "DFT.h"
 #include "OffAxFilter.h"
 
 // Create Pybind11 Module
 PYBIND11_MODULE(holo_lib, m) {
-     py::class_<DFT>(m, "DFT")
-        .def(py::init<py::array_t<int>, unsigned, unsigned, bool>())
-        .def("forwards", static_cast<bool (DFT::*)(py::array_t<complex<double>>)>(&DFT::forwards))
-        .def("backwards", static_cast<bool (DFT::*)(py::array_t<complex<double>>)>(&DFT::backwards))
-        .def("size", &DFT::size)
-        .def("threads_in_use", &DFT::threads_in_use);
-
     py::class_<OffAxFilter>(m, "OffAxFilter")
         .def(py::init<py::array_t<int, py::array::c_style | py::array::forcecast>, py::array_t<int, py::array::c_style | py::array::forcecast>, py::array_t<bool, py::array::c_style | py::array::forcecast>, 
                     py::array_t<complex<double>, py::array::c_style | py::array::forcecast>, py::array_t<double, py::array::c_style | py::array::forcecast>, unsigned, unsigned>())
