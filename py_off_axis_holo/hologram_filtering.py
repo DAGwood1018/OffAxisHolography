104fb8d059b90decd3be385dd3e0eb925819ad09
DAGwood1018 <43223204+DAGwood1018@users.noreply.github.com>
2026-06-26 14:11:02 -0400

    fixed bug in finding contrast

--- py_off_axis_holo/hologram_filtering.py
+++ py_off_axis_holo/hologram_filtering.py
@@ -442,7 +442,7 @@ class OffAxisFilter(DFT):
         self._ref = np.ones_like(ref)
         dc = self.__call__(fringes)
         self._ref = ref
-        return np.abs(dc)
+        return np.abs(dc)**2
 
     def contrast(self, fringes):
         """
@@ -454,7 +454,7 @@ class OffAxisFilter(DFT):
         :rtype: ndarray
         """
 
-        return 2 * np.abs(self.__call__(fringes)) / self.intensity(fringes)
+        return 2 * np.abs(self.__call__(fringes))**2 / self.intensity(fringes)
 
     def calibrate(self, fringes, dz=0, kNA=-1, roi=None, optimize=True, visualize=False, **kwargs):
         """
